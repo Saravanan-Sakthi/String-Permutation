77dcfd3525ff25c8ed6cce8628e52c6af8b4d113
Saravanan-Sakthi <saravanatesla@gmail.com>
2021-08-27 19:03:11 +0530

    27.08.2021

--- TestPermutation.java
+++ TestPermutation.java
@@ -3,9 +3,10 @@ import java.util.Scanner;
 class TestPermutation{
   public static void main(String [] abc){
     Scanner s= new Scanner(System.in);
+    System.out.print("Enter the string : ");
     String S= s.next();
-    Permutation p= new Permutation();
-    p.setsub(S);
+    Permutation p = new Permutation();
+    p.setSub(S);
     p.permute(0,S.length()-1);
   }
 }
